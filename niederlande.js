864ff3a957eeebfa57ac836de33dc98110778f57
Christian Weichsel <c.weichsel@gmx.de>
2023-10-03 19:44:25 +0200

    niederlande

---
+++ niederlande.js
@@ -0,0 +1,116 @@
+var niederlande = [
+  {
+    Jahr: 1906,
+    Saetze: [
+      {
+        Satz: "67-68",
+        marken: [
+          { Mn: 67, Besitz: "o", Doppelt: 0 },
+          { Mn: 68, Besitz: "o", Doppelt: 0 },
+        ],
+      },
+      {
+        Satz: "69-71",
+        marken: [
+          { Mn: 69, Besitz: "o", Doppelt: 0 },
+          { Mn: 70, Besitz: "o", Doppelt: 0 },
+          { Mn: 71, Besitz: "o", Doppelt: 0 },
+        ],
+      },
+    ],
+  },
+  {
+    Jahr: 1907,
+    Saetze: [
+        {
+          Satz: "72-74",
+          marken: [
+            { Mn: 72, Besitz: "o", Doppelt: 0 },
+            { Mn: 73, Besitz: "o", Doppelt: 0 },
+            { Mn: 74, Besitz: "o", Doppelt: 0 },
+          ],
+        },
+      ],
+  },
+  {
+    Jahr: 1908,
+    Saetze: [
+        {
+          Satz: "75-80",
+          marken: [
+            { Mn: 75, Besitz: "o", Doppelt: 0 },
+            { Mn: 76, Besitz: "o", Doppelt: 0 },
+            { Mn: 77, Besitz: "o", Doppelt: 0 },
+            { Mn: 78, Besitz: "o", Doppelt: 0 },
+            { Mn: 79, Besitz: "o", Doppelt: 0 },
+            { Mn: 80, Besitz: "o", Doppelt: 0 },
+          ],
+        },
+      ],
+  },
+  {
+    Jahr: 1913,
+    Saetze: [
+        {
+          Satz: "81-92",
+          marken: [
+            { Mn: 81, Besitz: "o", Doppelt: 0 },
+            { Mn: 82, Besitz: "o", Doppelt: 0 },
+            { Mn: 83, Besitz: "o", Doppelt: 0 },
+            { Mn: 84, Besitz: "o", Doppelt: 0 },
+            { Mn: 85, Besitz: "o", Doppelt: 0 },
+            { Mn: 86, Besitz: "", Doppelt: 0 },
+            { Mn: 87, Besitz: "", Doppelt: 0 },
+            { Mn: 88, Besitz: "", Doppelt: 0 },
+            { Mn: 89, Besitz: "", Doppelt: 0 },
+            { Mn: 90, Besitz: "", Doppelt: 0 },
+            { Mn: 91, Besitz: "", Doppelt: 0 },
+            { Mn: 92, Besitz: "", Doppelt: 0 },
+          ],
+        },
+      ],
+  },
+  {
+    Jahr: 1917,
+    Saetze: [
+        {
+          Satz: "93-94",
+          marken: [
+            { Mn: 93, Besitz: "o", Doppelt: 0 },
+            { Mn: 94, Besitz: "o", Doppelt: 0 },
+          ],
+        },
+      ],
+  },
+  {
+    Jahr: 1919,
+    Saetze: [
+        {
+          Satz: "95-96",
+          marken: [
+            { Mn: 95, Besitz: "o", Doppelt: 0 },
+            { Mn: 96, Besitz: "o", Doppelt: 0 },
+          ],
+        },
+      ],
+  },
+  {
+    Jahr: 1920,
+    Saetze: [
+        {
+          Satz: "97-98",
+          marken: [
+            { Mn: 97, Besitz: "o", Doppelt: 0 },
+            { Mn: 98, Besitz: "o", Doppelt: 0 },
+          ],
+        },
+        {
+            Satz: "99-100",
+            marken: [
+              { Mn: 99, Besitz: "", Doppelt: 0 },
+              { Mn: 100, Besitz: "", Doppelt: 0 },
+            ],
+          },
+      ],
+  },
+];
